7b4331511ede325f9d566564d61294a0641e42e5
Michael Lazarski <michael@lampeweb.de>
2021-02-07 16:47:56 +0100

    episode 6

--- playground/Playground.vue
+++ playground/Playground.vue
@@ -1,24 +1,80 @@
 <template>
   <div class="playground" data-theme="default">
-    Hello World
-    
-    <lcs-button>
-      Default
-    </lcs-button>
-    <lcs-button type="primary">
-      Primary
-    </lcs-button>
-    <lcs-button type="primary" block>
-      Primary Block
-    </lcs-button>
-    <lcs-button type="full">
-      Full
-    </lcs-button>
+    <h1>Theme Default</h1>
     <div>
-      <br>
-    <lcs-button type="plain">
-      Click Me maybe!
-    </lcs-button>
+      <lcs-button>
+        Default
+      </lcs-button>
+      <lcs-button type="primary">
+        Primary
+      </lcs-button>
+    </div>
+    <div>
+      <lcs-button type="primary" block>
+        Primary Block
+      </lcs-button>
+    </div>
+    <div>
+      <lcs-button type="full">
+        Full
+      </lcs-button>
+    </div>
+    <div>
+      <lcs-button type="plain">
+        Click Me maybe!
+      </lcs-button>
+    </div>
+  </div>
+  <div class="playground" data-theme="dark">
+    <h1>Theme dark</h1>
+    <div>
+      <lcs-button>
+        Default
+      </lcs-button>
+      <lcs-button type="primary">
+        Primary
+      </lcs-button>
+    </div>
+    <div>
+      <lcs-button type="primary" block>
+        Primary Block
+      </lcs-button>
+    </div>
+    <div>
+      <lcs-button type="full">
+        Full
+      </lcs-button>
+    </div>
+    <div>
+      <lcs-button type="plain">
+        Click Me maybe!
+      </lcs-button>
+    </div>
+  </div>
+    <div class="playground" data-theme="oled">
+    <h1>Theme oled</h1>
+    <div>
+      <lcs-button>
+        Default
+      </lcs-button>
+      <lcs-button type="primary">
+        Primary
+      </lcs-button>
+    </div>
+    <div>
+      <lcs-button type="primary" block>
+        Primary Block
+      </lcs-button>
+    </div>
+    <div>
+      <lcs-button type="full">
+        Full
+      </lcs-button>
+    </div>
+    <div>
+      <lcs-button type="plain">
+        Click Me maybe!
+      </lcs-button>
     </div>
   </div>
 </template>
@@ -26,13 +82,11 @@
 <script>
 import LcsButton from "@lcs-components/button";
 export default {
-    name: "playground",
-    components: {
-        LcsButton
-    }
-}
+  name: "playground",
+  components: {
+    LcsButton,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- playground/index.ts
+++ playground/index.ts
@@ -1,4 +1,5 @@
 import "@lcs-components/base/base.scss"
+import "@lcs-components/theme-default/theme.scss";
 
 import { createApp } from "vue";
 import Playground from "./Playground.vue";
